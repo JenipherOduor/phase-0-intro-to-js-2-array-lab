c17c1e14de97aa85a348aeaca9e2dabb48826bb0
Jenipher Oduor <jenipheroduor@gmail.com>
2023-07-02 11:52:07 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
+const cats = ["Milo", "Otis", "Garfield"]
 // Write your solution here!
+cats.push;
+
+function destructivelyAppendCat(name)
+{
+    cats.push(name);
+}
+function destructivelyPrependCat(name)
+{
+    cats.unshift(name)
+}
+function destructivelyRemoveLastCat(name)
+{
+    cats.pop(name)
+}
+function destructivelyRemoveFirstCat(name)
+{
+    cats.shift(name)
+}
+{
+    cats.append(name)
+}
+function appendCat(name)
+{
+    return [...cats, name]
+}
+
+function prependCat(name)
+{
+    return [name, ...cats]
+}
+function removeFirstCat()
+{
+    return cats.slice(1)
+}
+function removeLastCat()
+{
+    return cats.slice(0, cats.length - 1)
+}
